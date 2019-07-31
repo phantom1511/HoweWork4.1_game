4133d6c8e2115d5c0d5ffd7743c30f223ee636b0
Dastan Tulokulov <dastan.tulokulov.kg@mail.ru>
2019-07-31 17:25:03 +0600

    tankShoot метод добавляет по 20 рандомно каждому герою за исключением босса он на 0 индеске

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ import java.util.Random;
 
 public class Main {
 
-    public static int[] health = {800, 300, 300, 300, 600, 300, 300, 250}; //жизни нашего босса и героев
-    public static int[] damage = {50, 20, 20, 20, 10, 20, 20, 10}; //уроны которын могут нанести наши герои и босса
-    public static String[] damageType = {"Physical", "Physical", "Magical", "Mental", "Shoot", "Dodge", "Return", "Heal"};
-    public static String[] heroes = {"Boss", "Warrior", "Magical", "Mental", "Tank", "Flash", "Berserk", "Medic"};
+    public static int[] health = {1200, 300, 300, 300, 600, 300, 300, 300, 250}; //жизни нашего босса и героев
+    public static int[] damage = {50, 20, 20, 20, 10, 20, 20, 20, 10}; //уроны которын могут нанести наши герои и босса
+    public static String[] damageType = {"Physical", "Physical", "Magical", "Mental", "Shoot", "Dodge", "Return", "Thor", "Heal"};
+    public static String[] heroes = {"Boss", "Warrior", "Magical", "Mental", "Tank", "Flash", "Berserk", "Thunder", "Medic"};
 
     public static void main(String[] args) {
 
@@ -17,6 +17,7 @@ public class Main {
             tankShoot();
             flash();
             berserk();
+            thor();
             printStatistics();
         }
     }
@@ -28,7 +29,7 @@ public class Main {
     }
 
     public static void round() {
-        for (int i = 1; i <= 7; i++) {
+        for (int i = 1; i <= 8; i++) {
             int healthRemain = bossDamage(i);
             if (healthRemain < 0) {
                 health[i] = 0;
@@ -36,7 +37,7 @@ public class Main {
                 health[i] = healthRemain;
             }
         }
-        for (int i = 1; i <= 7; i++) {
+        for (int i = 1; i <= 8; i++) {
             int healthRemain = heroesDamage(i);
             if (healthRemain < 0) {
                 health[0] = 0;
@@ -44,7 +45,7 @@ public class Main {
                 health[0] = healthRemain;
             }
         }
-        for (int i = 1; i <= 6; i++) {
+        for (int i = 1; i <= 7; i++) {
             if (health[7] > 0) {
                 health[i] = medicHeal(i);
             } else {
@@ -62,6 +63,18 @@ public class Main {
         System.out.println(damage[6] + "----------------------------------");
     }
 
+    public static void thor() {
+        int yes = 2;
+        int no = 0;
+        int thunder = no + (int) (Math.random() * yes);
+        System.out.println(thunder);
+        if (thunder == 0) {
+            damage[0] = 0;
+        } else {
+            damage[0] = 50;
+        }
+    }
+
     public static void flash() {
         int yes = 2;
         int no = 0;
@@ -74,14 +87,14 @@ public class Main {
 
     public static void tankShoot() {
         int from = 1;
-        int to = 7;
+        int to = 8;
         int randomHero = from + (int) (Math.random() * to);
         System.out.println("Tank blocked " + randomHero + " damage");
         health[randomHero] = health[randomHero] += 20;
     }
 
     public static int medicHeal(int heroIndex) {
-        return health[heroIndex] + damage[7];
+        return health[heroIndex] + damage[8];
     }
 
     public static int bossDamage(int heroIndex) {
@@ -108,7 +121,7 @@ public class Main {
             System.out.println("CONGRATULATIONS!!");
             return true;
         }
-        if (health[1] <= 0 && health[2] <= 0 && health[3] <= 0 && health[4] <= 0 && health[5] <= 0 && health[6] <= 0 && health[7] <= 0) {
+        if (health[1] <= 0 && health[2] <= 0 && health[3] <= 0 && health[4] <= 0 && health[5] <= 0 && health[6] <= 0 && health[7] <= 0 && health[8] <= 0) {
             System.out.println("Boss won the game!!");
             System.out.println("GAME OVER");
             return true;
@@ -126,7 +139,8 @@ public class Main {
         System.out.println("Tank health: " + health[4]);
         System.out.println("Flash health: " + health[5]);
         System.out.println("Berserk health: " + health[6]);
-        System.out.println("Medic health: " + health[7]);
+        System.out.println("Thor health: " + health[7]);
+        System.out.println("Medic health: " + health[8]);
         System.out.println("................................");
     }
 }
